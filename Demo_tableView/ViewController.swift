06d091bf2eec9022899576f2d88b489cd8b2780c
HungChita <hungchita@hungchitademacbook-air.local>
2016-04-09 02:02:32 +0800

    Initial Commit

---
+++ Demo_tableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo_tableView
+//
+//  Created by HungChita on 2016/4/9.
+//  Copyright © 2016年 HungChita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
